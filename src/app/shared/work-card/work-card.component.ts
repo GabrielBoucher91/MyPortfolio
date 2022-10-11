27984b7938a618b6c998bb4c705ea629f07732bf
Gabriel Boucher <gab.boucher91@gmail.com>
2022-10-10 21:04:05 -0400

    Parametrized the work-card component and fixed some routing issues in the about page

--- src/app/shared/work-card/work-card.component.ts
+++ src/app/shared/work-card/work-card.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-work-card',
   templateUrl: './work-card.component.html',
-  styleUrls: ['./work-card.component.scss']
+  styleUrls: ['./work-card.component.scss'],
 })
 export class WorkCardComponent implements OnInit {
+  @Input() title: string;
+  @Input() company: string;
+  @Input() startDate: string;
+  @Input() endDate: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
